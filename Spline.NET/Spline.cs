0c052b0c6281c8ca4214938275904d333f34849a
Lando Seidel <lsvs2@outlook.de>
2019-08-09 12:32:11 +0200

    Init

---
+++ Spline.NET/Spline.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using MathNet.Spatial.Euclidean;
+
+namespace Spline.NET
+{
+    public static class Spline
+    {
+        /// <summary>
+        /// De Boor Recursion Algorithm
+        /// </summary>
+        /// <param name="i">index</param>
+        /// <param name="p">degree</param>
+        /// <param name="t">knot vector</param>
+        /// <param name="x">variable</param>
+        /// <returns>Result</returns>
+        public static double B(int i, int p, double[] t, double x)
+        {
+            if (p == 0)
+            {
+                if (t[i] <= x && x < t[i + 1])
+                {
+                    return 1.0;
+                }
+                else
+                {
+                    return 0.0;
+                }
+            }
+            else
+            {
+                double t1;
+                double t2;
+                if (t[i + p] - t[i] == 0)
+                {
+                    t1 = 0;
+                }
+                else
+                {
+                    t1 = (x - t[i]) / (t[i + p] - t[i]);
+                }
+                if (t[i + p + 1] - t[i + 1] == 0)
+                {
+                    t2 = 0;
+                }
+                else
+                {
+                    t2 = (t[i + p + 1] - x) / (t[i + p + 1] - t[i + 1]);
+                }
+                return t1 * B(i, p - 1, t, x) + t2 * B(i + 1, p - 1, t, x);
+            }
+        }
+
+        /// <summary>
+        /// Evaluate Spline at x
+        /// </summary>
+        /// <param name="P">Control Points</param>
+        /// <param name="t">Knot Values</param>
+        /// <param name="p">Degree of Spline</param>
+        /// <param name="x">Evaluated at this parameter</param>
+        /// <returns>Evaluated Point</returns>
+        public static Point2D EvaluateSpline(Point2D[] P, double[] t, int p, double x)
+        {
+            double n = t.Length;
+            double[] cx = new double[P.Length];
+            double[] cy = new double[P.Length];
+            double xt = 0.0;
+            double yt = 0.0;
+            for (int i = 0; i < P.Length; i++)
+            {
+                cx[i] = P[i].X;
+                cy[i] = P[i].Y;
+            }
+            for (int i = 0; i <= n - p - 2; i++)
+            {
+                xt += cx[i] * B(i, p, t, x);
+                yt += cy[i] * B(i, p, t, x);
+            }
+            return new Point2D(xt, yt);
+        }
+    }
+}
---
+++ Spline.Test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IxMilia.Dxf;
+using IxMilia.Dxf.Entities;
+using MathNet.Numerics.Interpolation;
+using MathNet.Spatial.Euclidean;
+
+namespace Spline.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DxfFile dxfFile = DxfFile.Load(@"D:\Test\Spline.dxf");
+            IList<DxfEntity> dxfEntities = dxfFile.Entities;
+            foreach (var entity in dxfEntities)
+            {
+                if (entity is DxfSpline)
+                {
+                    DxfSpline dxfSpline = entity as DxfSpline;
+                    Point2D[] p = new Point2D[dxfSpline.ControlPoints.Count];
+                    double[] t = new double[dxfSpline.KnotValues.Count];
+                    for (int i = 0; i < p.Length; i++)
+                    {
+                        p[i] = new Point2D(dxfSpline.ControlPoints[i].X, dxfSpline.ControlPoints[i].Y);
+                    }
+                    for (int i = 0; i < t.Length; i++)
+                    {
+                        t[i] = dxfSpline.KnotValues[i];
+                    }
+                    Point2D test = new Point2D(dxfSpline.ControlPoints[0].X, dxfSpline.ControlPoints[0].Y);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.1);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.2);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.3);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.4);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.5);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.6);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.7);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.8);
+                    test = NET.Spline.EvaluateSpline(p, t, dxfSpline.DegreeOfCurve, 0.9);
+                    test = new Point2D(dxfSpline.ControlPoints[dxfSpline.ControlPoints.Count - 1].X, dxfSpline.ControlPoints[dxfSpline.ControlPoints.Count - 1].Y);
+                }
+            }
+        }
+    }
+}
